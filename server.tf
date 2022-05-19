5d4705ca3390175dda988517e3a60a4a2631474a
gbvraju <gbvraju@gmail.com>
2022-05-19 17:11:42 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "myawsserver" {
   tags = {
     Name = "gbvraju-devops-server-v2"
     env = "production"
-    owner = "Gagandeep"
+    owner = "Gbvraju"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
